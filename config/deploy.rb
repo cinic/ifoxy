0f744207f2bd1c0a3ff172c1c401c049d12162f8
Alexander Andreev <cinic.rus@gmail.com>
2015-01-25 04:09:18 +0300

    bump version

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,23 +67,26 @@ end
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+  # desc 'Restart application'
+  #task :restart do
+  #  on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+  #  end
+  #end
 
-  after :publishing, :restart
+  #after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
+  #after :restart, :clear_cache do
+  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #    # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
-    end
-  end
+  #  end
+  #end
+  after :finishing, "deploy:cleanup"
+  after :finishing, "foreman:export"
+  after :finishing, "foreman:restart"
 
 end
